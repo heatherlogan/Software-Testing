ad06f8bdcc5ea3ae99efe9053b4256cfbec821c3
heather_logan <s1423498@sms.ed.ac.uk>
2018-03-27 12:38:01 +0100

    testing push

---
+++ Task2_Mutation.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Task2_Mutation {
+	
+	// Need to add all tests together here from other repo 
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Task2_TDD_2.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Task2_TDD_2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
